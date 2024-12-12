03fdc08fdb3410a65c066024f1ac8f1cca611c11
ghiasyalexandre <ghiasyalexandre@gmail.com>
2024-12-11 21:57:48 -0500

    Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StoryComponent } from './story/story.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StoryComponent, DetailsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'anne-wedding';
