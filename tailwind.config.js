124880a79e1c3d0dc63e07f93f62fa85ba6a3214
ghiasyalexandre <ghiasyalexandre@gmail.com>
2025-01-16 00:23:29 -0500

    RSVP Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '480px',
+      },
       backgroundImage: {
         'paper-texture': "url('https://static.wixstatic.com/media/11062b_0e4baa071ef74597b23e6d8e5afcb19ef000.jpg/v1/fill/w_980,h_846,al_c,q_85,usm_0.33_1.00_0.00,enc_avif,quality_auto/11062b_0e4baa071ef74597b23e6d8e5afcb19ef000.jpg')",
 
