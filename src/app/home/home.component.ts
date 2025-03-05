bfa1cd7676fd84b0ebfd5c7fbd40daef763cb469
ghiasyalexandre <ghiasyalexandre@gmail.com>
2025-03-04 21:10:44 -0500

    Countdown on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CountdownComponent } from './../countdown/countdown.component';
 import { Component } from '@angular/core';
 import { StoryComponent } from '../story/story.component';
 import { CommonModule } from '@angular/common';
@@ -6,7 +7,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StoryComponent, CommonModule, RouterModule],
+  imports: [StoryComponent, CommonModule, RouterModule, CountdownComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
